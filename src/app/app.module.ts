7a8cf3a5af4c23d849be8d45e1bd9eb50367ff54
Abraao <abraaoayrs@gmail.com>
2020-04-17 17:00:44 -0300

    Atualização para o Angular 4.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -39,7 +39,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     NotFoundComponent,
   ],
   imports: [
-    HttpModule,
+    HttpClientModule,
     FormsModule,
     SharedModule.forRoot(),
     BrowserModule,
